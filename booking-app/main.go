be1cd920e586f4ae0ff3cf636ddce7c893c9429e
Qinchi <raify.zhao@gmail.com>
2022-10-24 09:45:13 +0800

    update go booking-app
    Signed-off-by: Qinchi <raify.zhao@gmail.com>

---
+++ booking-app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("welecom to our conference booking application")
+	fmt.Println("Get your tickets here to attend")
+
+	var name = "Go Conference"
+
+}
