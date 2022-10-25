d1ae8ce94815292ac7e23f27ac949a24b76b7067
Qinchi <raify.zhao@gmail.com>
2022-10-25 09:26:48 +0800

    var const Printf
    Signed-off-by: Qinchi <raify.zhao@gmail.com>

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,9 +4,16 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("welecom to our conference booking application")
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("welecom to our %v booking application\n", conferenceName)
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets here to attend")
 
-	var name = "Go Conference"
+	//var name = "Go Conference"
+	//Println = Print +\n
+	//Printf  format for %
 
 }
