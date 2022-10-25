78596c569ccd29c6c17eb1ec76e4079026f5aa6f
Qinchi <raify.zhao@gmail.com>
2022-10-25 09:44:50 +0800

    format %v %T type int string
    Signed-off-by: Qinchi <raify.zhao@gmail.com>

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,16 +4,31 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	//var conferenceName string = "Go Conference" equal conferenceName := "Go Conference"
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	// %v replace for varable
+	// %T replace for type
+
+	fmt.Printf("conferenceTickets is %T, remaining is %T, coonferenceName is %T\n", conferenceTickets, conferenceName, remainingTickets)
 
 	fmt.Printf("welecom to our %v booking application\n", conferenceName)
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("We have total of %v tickets and  %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	//var name = "Go Conference"
 	//Println = Print +\n
-	//Printf  format for %
+	//Printf  format for varable
+
+	var userName string
+	var userTickets int
+
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 
 }
